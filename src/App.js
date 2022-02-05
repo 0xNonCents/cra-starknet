3a41ac3dba0e2a8e8f8a82200b9389340d5b1f8a
Matthew Demoy <0xNonCents@gmail.com>
2022-02-05 11:17:04 -0500

    add starknet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import starknet from "starknet";
 
 function App() {
+  const isHex = starknet.number.isHex("0x1");
   return (
     <div className="App">
       <header className="App-header">
